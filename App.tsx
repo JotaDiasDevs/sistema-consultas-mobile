cfcbd52245023dba2bb873e8649398c3489cfd1f
JotaDiasDevs <joaovitor.semente.d@gmail.com>
2026-02-26 22:08:01 -0300

    implementação do MVP inicial com React Native

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}> Consultas Online</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo: {
+    fontSize: 30,
+    flex: 1,
+    
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
